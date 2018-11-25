efbc911aeb126c7123dd044de7c383e5f25e0da2
Rainer Burgstaller <rainer.burgstaller@e-mundo.de>
2018-11-25 14:23:25 +0100

    initial project showing the problem

---
+++ src/Application.kt
@@ -0,0 +1,39 @@
+package com.example
+
+import com.ryanharter.ktor.moshi.moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import java.util.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    install(ContentNegotiation) {
+        moshi {
+            add(Date::class.java, Rfc3339DateJsonAdapter())
+        }
+    }
+
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
